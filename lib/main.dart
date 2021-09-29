ea572e3548d1fddd94100f458732131551180c44
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-09-29 13:37:55 +0300

    Added Map for question and answers

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,27 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
-      'What\'s your favorite song?',
-      'What\'s your favorite singer?',
+      {
+        'questionText':'What\'s your favorite color?',
+        'answers' : ['Blue','Red','Green','Blue']
+      },
+      {
+        'questionText':'What\'s your favorite animal?',
+        'answers' : ['Lion','Snake','Elephant','Goat']
+      },
+      {
+        'questionText':'What\'s your favorite team?',
+        'answers' : ['Chelsea','Arsenal','Barcelona','Real Madrid']
+      },
+      {
+        'questionText':'What\'s your favorite book?',
+        'answers' : ['Gulliver’s Travels','Frankenstein','Alice’s Adventures in Wonderland',' The Way We Live Now']
+      }
     ];
 
     void answerQuestion() {
       setState(() {
-        questionIndex = questionIndex +
-            1; //changes question on the screen with StatefulWidget
+        questionIndex = (questionIndex + 1) % 4; //changes question on the screen with StatefulWidget
       });
     }
 
@@ -39,19 +50,10 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[questionIndex]),
-            Answer(
-              'answer 1',
-              answerQuestion,
-            ),
-            Answer(
-              'answer 2',
-              answerQuestion,
-            ),
-            Answer(
-              'answer 3',
-              answerQuestion,
-            ),
+            Question(questions[questionIndex]['questionText'] as String),
+            ...(questions[questionIndex]['answers'] as List<String>).map((answer){
+               return Answer(answer,answerQuestion);
+            }).toList()
           ],
         ),
       ),
