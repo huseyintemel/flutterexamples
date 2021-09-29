aedba51fd79987a4e1875a9a990e55d320feaf9a
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-09-29 10:51:04 +0300

    Added some style to question widget

--- lib/question.dart
+++ lib/question.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-
   final String questionText;
 
   const Question(this.questionText); //Question Constructor
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity, //width of screen
+      margin: EdgeInsets.all(20), // margin from all directions
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
