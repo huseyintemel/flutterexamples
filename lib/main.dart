f8d661ab8d0482447764f36adf1c55eae26b8c58
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-09-29 16:16:15 +0300

    Converted quiz and result to widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,26 +20,26 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     var questions = [
       {
-        'questionText':'What\'s your favorite color?',
-        'answers' : ['Blue','Red','Green','Blue']
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Blue', 'Red', 'Green', 'Blue']
       },
       {
-        'questionText':'What\'s your favorite animal?',
-        'answers' : ['Lion','Snake','Elephant','Goat']
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Lion', 'Snake', 'Elephant', 'Goat']
       },
       {
-        'questionText':'What\'s your favorite team?',
-        'answers' : ['Chelsea','Arsenal','Barcelona','Real Madrid']
+        'questionText': 'What\'s your favorite team?',
+        'answers': ['Chelsea', 'Arsenal', 'Barcelona', 'Real Madrid']
       },
       {
-        'questionText':'What\'s your favorite book?',
-        'answers' : ['Gulliver’s Travels','Frankenstein','Alice’s Adventures in Wonderland',' The Way We Live Now']
+        'questionText': 'What\'s your favorite book?',
+        'answers': ['Gulliver’s Travels','Frankenstein','Alice’s Adventures in Wonderland',' The Way We Live Now']
       }
     ];
 
     void answerQuestion() {
       setState(() {
-        questionIndex = questionIndex + 1; //changes question on the screen with StatefulWidget
+        questionIndex += 1; //changes question on the screen with StatefulWidget
       });
     }
 
@@ -48,14 +48,13 @@ class MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: questionIndex < questions.length ? Column(
-          children: [
-            Question(questions[questionIndex]['questionText'] as String),
-            ...(questions[questionIndex]['answers'] as List<String>).map((answer){
-               return Answer(answer,answerQuestion);
-            }).toList()
-          ],
-        ) : const Center(child: Text('Questions Finished'),),
+        body: questionIndex < questions.length
+            ? Quiz(
+                questions: questions,
+                answerQuestion: answerQuestion,
+                questionIndex: questionIndex,
+              )
+            : Result()
       ),
     );
   }
