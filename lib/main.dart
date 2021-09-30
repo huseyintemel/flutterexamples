e38adcc27e70bdaa62300437d96ac1792a3a803d
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-09-30 17:05:52 +0300

    Added new example flutter project
    In this example user can add transaction to list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import './quiz.dart';
 import './result.dart';
@@ -55,6 +56,42 @@ class MyAppState extends State<MyApp> {
                 questionIndex: questionIndex,
               )
             : Result()
+      ),
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+import './widgets/user_transactions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            UserTransactions(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTransaction;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTransaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                addTransaction(titleController.text,double.parse(amountController.text));
+              },
+              child: const Text('Add Transaction'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
