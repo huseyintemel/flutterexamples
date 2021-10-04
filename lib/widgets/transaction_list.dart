041147cc2ec1defc8bb5a6c0b0b0a117db74c916
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-10-04 12:51:13 +0300

    Added ListTile to transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -24,49 +24,24 @@ class TransactionList extends StatelessWidget {
       )
       : ListView.builder(itemBuilder: (context,index){
         return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
+          elevation: 5,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 6),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
                 padding: const EdgeInsets.all(10),
-                child: Text(
-                  '\$${transactions[index].amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
+                child: FittedBox(child: Text('\$${transactions[index].amount.toStringAsFixed(2)}')),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transactions[index].title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    DateFormat('dd-MM-yyyy').format(transactions[index].date),
-                    style: const TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
+            ),
+            title: Text(transactions[index].title,style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            subtitle: 
+            Text(
+              DateFormat('dd-MM-yyyy').format(transactions[index].date),
+              style: const TextStyle(
+                color: Colors.grey,
               ),
-            ],
+            ) 
           ),
         );
       },
